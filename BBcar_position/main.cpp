5d4d77e1f3b705e8a06fd1a299cb05e5d58d80a6
Landy <landychen1111@gmail.com>
2021-06-09 10:23:47 -0700

    add ping

--- BBcar_position/main.cpp
+++ BBcar_position/main.cpp
@@ -1,28 +1,46 @@
 #include"mbed.h"
 #include "bbcar.h"
-#include "bbcar_rpc.h"
 
-BufferedSerial pc(USBTX,USBRX); //tx,rx
+
+// BufferedSerial pc(USBTX,USBRX); //tx,rx
 
 Ticker servo_ticker;
 DigitalOut led1(LED1);
 BufferedSerial uart(D1,D0); //tx,rxss
-// static BufferedSerial pc(STDIO_UART_TX, STDIO_UART_RX);
-// DigitalInOut ping(D10);
+static BufferedSerial pc(STDIO_UART_TX, STDIO_UART_RX);
 DigitalInOut pin10(D10);
-parallax_ping  ping1(pin10);
+
 PwmOut pin5(D5), pin6(D6);
 BBCar car(pin5, pin6, servo_ticker);
 
 int main(){
    uart.set_baud(9600);
-   printf("Start!\n");
+   parallax_ping  ping1(pin10);
    while(1){
-      if(uart.readable()){
-            char recv[1];
-            uart.read(recv, sizeof(recv));
-            pc.write(recv, sizeof(recv));
-            // printf("%s\n", recv);
-      }else{printf("bad\n");}
+      printf("Ping = %lf\r\n", (float)ping1);
+      if(uart.readable() && (float)ping1>20){
+         char recv[1];
+         uart.read(recv, sizeof(recv));
+         if(recv[0] == 'g') {
+            printf("go straight!\n");
+            car.goStraight(50, 1, 1);
+            ThisThread::sleep_for(1000ms);
+            car.stop();
+         }
+         else if(recv[0] == 'l') {
+            printf("turn left!\n");
+            car.turn(30,-0.3);
+            ThisThread::sleep_for(1000ms);
+            car.stop();
+            // ThisThread::sleep_for(500ms);
+         }
+         else if(recv[0] == 'r') {
+            printf("Turn right!\n");
+            car.turn(30, 0.3);
+            ThisThread::sleep_for(1000ms);
+            car.stop();
+            // ThisThread::sleep_for(500ms);
+         }
+      }
    }
 }
