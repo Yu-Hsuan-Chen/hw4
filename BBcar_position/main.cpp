e40911f08cd3c356ef6f91d97119295b7681b01b
Landy <landychen1111@gmail.com>
2021-06-08 23:20:52 -0700

    add hw4-3 car position

---
+++ BBcar_position/main.cpp
@@ -0,0 +1,58 @@
+#include"mbed.h"
+#include "bbcar.h"
+
+BufferedSerial uart(D1,D0); //tx,rx
+static BufferedSerial pc(STDIO_UART_TX, STDIO_UART_RX);
+DigitalInOut pin10(D10);
+parallax_ping  ping1(pin10);
+PwmOut pin5(D5), pin6(D6);
+BBCar car(pin5, pin6, servo_ticker);
+
+Timer t;
+Ticker servo_ticker;
+
+int main(){
+   uart.set_baud(9600);
+   bool stop = false;
+   double distance = 0;
+   float val;
+   //pc.set_baud(9600);
+   while(1){
+        ping.output();
+        ping = 0; wait_us(200);
+        ping = 1; wait_us(5);
+        ping = 0; wait_us(5);
+        ping.input();
+        while(ping.read() == 0);
+        t.start();
+        while(ping.read() == 1);
+        val = t.read();
+        printf("Ping = %lf\r\n", val*17700.4f);
+        distance = val*17700.4f;
+        t.stop();
+        t.reset();
+
+        if(uart.readable() && distance > 20){
+            char recv[1];
+            uart.read(recv, sizeof(recv));
+            if(recv[0] == 'f') {
+               car.goStraight(100, 0.9, 1);
+               ThisThread::sleep_for(1000ms);
+               car.stop();
+               
+            }
+            else if(recv[0] == 'l' ) {
+               car.turn(50,0.3);
+               ThisThread::sleep_for(1000ms);
+               car.stop();
+            }
+            else if(recv[0] == 'r' ) {
+               car.turn(50,-0.3);
+               ThisThread::sleep_for(1000ms);
+               car.stop();
+            }
+        }
+   }
+}
+
+
