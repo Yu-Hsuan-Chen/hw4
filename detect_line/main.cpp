1ec2d4f389cd27071678742f47947169cd5db3d2
Yu-Hsuan-Chen <68105852+Yu-Hsuan-Chen@users.noreply.github.com>
2021-06-10 23:32:38 +0800

    Update main.cpp

--- detect_line/main.cpp
+++ detect_line/main.cpp
@@ -1,6 +1,8 @@
-#include"mbed.h"
+#include "mbed.h"
 #include "bbcar.h"
 #include "bbcar_rpc.h"
+
+
 Ticker servo_ticker;
 PwmOut pin5(D5), pin6(D6);
 
@@ -9,40 +11,20 @@ BBCar car(pin5, pin6, servo_ticker);
 BufferedSerial pc(USBTX,USBRX); //tx,rx
 BufferedSerial uart(D1,D0); //tx,rx
 
-int main(){
+
+int main() {
    char buf[256], outbuf[256];
    uart.set_baud(9600);
    while(1){
-      memset(buf, 0, 256);
-      for( int i = 0; ; i++ ) {
-         if(uart.readable()){
-            char recv[1];
-            uart.read(recv, sizeof(recv));
-            buf[i] = recv;
+      for (int i=0; ; i++) {
+         char *recv = new char[1];
+         uart.read(recv, 1);
+         buf[i] = *recv;
+         if (*recv == '\n') {
+         break;
          }
-         else
-            break;   
       }
+      printf("%s\r\n", buf);
       RPC::call(buf, outbuf);
    }
 }
-
-
-
-// int main() {
-//    char buf[256], outbuf[256];
-//    FILE *devin = fdopen(&xbee, "r");
-//    FILE *devout = fdopen(&xbee, "w");
-//    while (1) {
-//       memset(buf, 0, 256);
-//       for( int i = 0; ; i++ ) {
-//          char recv = fgetc(devin);
-//          if(recv == '\n') {
-//             printf("\r\n");
-//             break;
-//          }
-//          buf[i] = fputc(recv, devout);
-//       }
-//    RPC::call(buf, outbuf);
-//    }
-// }
